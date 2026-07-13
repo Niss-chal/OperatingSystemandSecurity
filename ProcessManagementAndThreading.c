ffb79688e6f3c5653004b67d4832df4bc6e7abb2
Niss-chal <240378@softwarica.edu.np>
2026-07-13 22:02:01 +0545

    Added mutex and semaphores as synchronization mechanisms

--- ProcessManagementAndThreading.c
+++ ProcessManagementAndThreading.c
@@ -1,33 +1,63 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <unistd.h>
 
-void *threadOne(void *arg) {
-    printf("Thread 1 is checking system information...\n");
+int completedTasks = 0;
+
+pthread_mutex_t lock;
+sem_t semaphore;
+
+void *threadOne(void *arg)
+{
+    printf("Thread 1 is checking system information\n");
     sleep(1);
+
+    pthread_mutex_lock(&lock);
+    completedTasks++;
+    pthread_mutex_unlock(&lock);
+
     printf("Thread 1 completed\n");
 
-    pthread_exit(NULL);
+    sem_post(&semaphore);
+
+    return NULL;
 }
 
-void *threadTwo(void *arg) {
-    printf("Thread 2 is checking memory...\n");
+void *threadTwo(void *arg)
+{
+    printf("Thread 2 is checking memory\n");
     sleep(1);
+
+    pthread_mutex_lock(&lock);
+    completedTasks++;
+    pthread_mutex_unlock(&lock);
+
     printf("Thread 2 completed\n");
 
-    pthread_exit(NULL);
+    sem_post(&semaphore);
+
+    return NULL;
 }
 
-void *threadThree(void *arg) {
-    printf("Thread 3 is scheduling processes...\n");
+void *threadThree(void *arg)
+{
+    sem_wait(&semaphore);
+    sem_wait(&semaphore);
+
+    printf("Thread 3 is scheduling processes\n");
     sleep(1);
     printf("Thread 3 completed\n");
 
-    pthread_exit(NULL);
+    return NULL;
 }
 
-int main() {
+int main()
+{
     pthread_t thread1, thread2, thread3;
+
+    pthread_mutex_init(&lock, NULL);
+    sem_init(&semaphore, 0, 0);
 
     pthread_create(&thread1, NULL, threadOne, NULL);
     pthread_create(&thread2, NULL, threadTwo, NULL);
@@ -37,7 +67,11 @@ int main() {
     pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
 
+    printf("Completed tasks: %d\n", completedTasks);
     printf("All threads completed\n");
+
+    pthread_mutex_destroy(&lock);
+    sem_destroy(&semaphore);
 
     return 0;
 }
