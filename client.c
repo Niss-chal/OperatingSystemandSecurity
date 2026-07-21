0e1f82ba64d0ff0d41abacc50b741e2ca2332b37
Niss-chal <240378@softwarica.edu.np>
2026-07-21 23:42:38 +0545

    made server and client and exchanged message between them

---
+++ client.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int clientSocket;
+    struct sockaddr_in serverAddress;
+    char message[100];
+
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(clientSocket,
+            (struct sockaddr *)&serverAddress,
+            sizeof(serverAddress));
+
+    strcpy(message, "Hello from client");
+
+    send(clientSocket, message, strlen(message) + 1, 0);
+
+    read(clientSocket, message, sizeof(message));
+
+    printf("Message from server: %s\n", message);
+
+    close(clientSocket);
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddress;
+    char message[100];
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSocket,
+         (struct sockaddr *)&serverAddress,
+         sizeof(serverAddress));
+
+    listen(serverSocket, 1);
+
+    printf("Server is waiting for client...\n");
+
+    clientSocket = accept(serverSocket, NULL, NULL);
+
+    read(clientSocket, message, sizeof(message));
+
+    printf("Message from client: %s\n", message);
+
+    strcpy(message, "Hello from server");
+
+    send(clientSocket, message, strlen(message) + 1, 0);
+
+    close(clientSocket);
+    close(serverSocket);
+
+    return 0;
+}
