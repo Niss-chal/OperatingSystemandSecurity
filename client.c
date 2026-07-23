579a37472aa67bb6d7fec709cbf97e57468f91ec
Niss-chal <240378@softwarica.edu.np>
2026-07-23 23:25:22 +0545

    Implemented client server authentication with proper error handling and handling multiple concurrent connections

--- client.c
+++ client.c
@@ -8,22 +8,77 @@ int main()
     int clientSocket;
     struct sockaddr_in serverAddress;
     char message[100];
+    char username[30];
+    char password[30];
+    char login[100];
 
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (clientSocket < 0)
+    {
+        printf("Socket creation failed\n");
+        return 1;
+    }
 
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    connect(clientSocket,
-            (struct sockaddr *)&serverAddress,
-            sizeof(serverAddress));
+    if (connect(clientSocket,
+                (struct sockaddr *)&serverAddress,
+                sizeof(serverAddress)) < 0)
+    {
+        printf("Connection failed\n");
+        close(clientSocket);
+        return 1;
+    }
 
-    strcpy(message, "MSG:Hello from client side!!");
+    printf("Enter username: ");
+    scanf("%30s", username);
 
-    send(clientSocket, message, strlen(message) + 1, 0);
+    printf("Enter password: ");
+    scanf("%30s", password);
 
-    read(clientSocket, message, sizeof(message));
+    sprintf(login, "%s:%s", username, password);
+
+    if (send(clientSocket, login, strlen(login) + 1, 0) < 0)
+    {
+        printf("Login details could not be sent\n");
+        close(clientSocket);
+        return 1;
+    }
+
+    if (read(clientSocket, message, sizeof(message)) <= 0)
+    {
+        printf("Authentication response not received\n");
+        close(clientSocket);
+        return 1;
+    }
+
+    if (strcmp(message, "AUTH_SUCCESS") != 0)
+    {
+        printf("Authentication failed\n");
+        close(clientSocket);
+        return 1;
+    }
+
+    printf("Authentication successful\n");
+
+    strcpy(message, "MSG:Hello from client side!!!");
+
+    if (send(clientSocket, message, strlen(message) + 1, 0) < 0)
+    {
+        printf("Message could not be sent\n");
+        close(clientSocket);
+        return 1;
+    }
+
+    if (read(clientSocket, message, sizeof(message)) <= 0)
+    {
+        printf("Server response not received\n");
+        close(clientSocket);
+        return 1;
+    }
 
     printf("Server response: %s\n", message);
 
--- server.c
+++ server.c
@@ -1,47 +1,133 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
+
+void *handleClient(void *arg)
+{
+    int clientSocket = *(int *)arg;
+    char login[100];
+    char message[100];
+
+    free(arg);
+
+    if (read(clientSocket, login, sizeof(login)) <= 0)
+      {
+        printf("Login details could not be received\n");
+        close(clientSocket);
+        return NULL;
+      }
+
+    if (strcmp(login, "admin:linux") != 0)
+      {
+        strcpy(message, "AUTH_FAILED");
+        send(clientSocket, message, strlen(message) + 1, 0);
+
+        printf("Client authentication failed\n");
+        close(clientSocket);
+        return NULL;
+      }
+    strcpy(message, "AUTH_SUCCESS");
+
+    if (send(clientSocket, message, strlen(message) + 1, 0) < 0)
+    {
+        printf("Authentication response failed\n");
+        close(clientSocket);
+        return NULL;
+    }
+
+    if (read(clientSocket, message, sizeof(message)) <= 0)
+    {
+        printf("Message could not be received\n");
+        close(clientSocket);
+        return NULL;
+    }
+
+    printf("Data received: %s\n", message);
+
+    if (strncmp(message, "MSG:", 4) == 0 &&
+        strlen(message) > 4)
+    {
+        strcpy(message, "ACK:Message received from server side!!!");
+    }
+    else
+    {
+        strcpy(message, "ERROR:Invalid message");
+    }
+
+    if (send(clientSocket, message, strlen(message) + 1, 0) < 0)
+    {
+        printf("Response could not be sent\n");
+    }
+
+    close(clientSocket);
+    return NULL;
+}
 
 int main()
 {
-    int serverSocket, clientSocket;
+    int serverSocket;
     struct sockaddr_in serverAddress;
-    char message[100];
 
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serverSocket < 0)
+    {
+        printf("Socket creation failed\n");
+        return 1;
+    }
 
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    bind(serverSocket,
-         (struct sockaddr *)&serverAddress,
-         sizeof(serverAddress));
-
-    listen(serverSocket, 1);
-
-    printf("Server is waiting for client...\n");
-
-    clientSocket = accept(serverSocket, NULL, NULL);
-
-    read(clientSocket, message, sizeof(message));
-
-    printf("Data received: %s\n", message);
-
-    if (strncmp(message, "MSG:", 4) == 0)
+    if (bind(serverSocket,
+             (struct sockaddr *)&serverAddress,
+             sizeof(serverAddress)) < 0)
     {
-        strcpy(message, "ACK:Message received");
-    }
-    else
-    {
-        strcpy(message, "ERROR:Invalid protocol");
+        printf("Bind failed\n");
+        close(serverSocket);
+        return 1;
     }
 
-    send(clientSocket, message, strlen(message) + 1, 0);
+    if (listen(serverSocket, 5) < 0)
+    {
+        printf("Listen failed\n");
+        close(serverSocket);
+        return 1;
+    }
 
-    close(clientSocket);
+    printf("Server is waiting for clients...\n");
+
+    while (1)
+    {
+        int *clientSocket = malloc(sizeof(int));
+
+        *clientSocket = accept(serverSocket, NULL, NULL);
+
+        if (*clientSocket < 0)
+        {
+            printf("Client connection failed\n");
+            free(clientSocket);
+            continue;
+        }
+
+        pthread_t thread;
+
+        if (pthread_create(&thread, NULL,
+                           handleClient, clientSocket) != 0)
+        {
+            printf("Thread creation failed\n");
+            close(*clientSocket);
+            free(clientSocket);
+            continue;
+        }
+
+        pthread_detach(thread);
+    }
+
     close(serverSocket);
-
     return 0;
 }
